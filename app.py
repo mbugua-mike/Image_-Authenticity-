21f394c2857535e0374bd541e719cd3cb7f4f0b6
mbugua-mike <110477020+mbugua-mike@users.noreply.github.com>
2025-04-27 06:45:03 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -162,4 +162,4 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=False) 
